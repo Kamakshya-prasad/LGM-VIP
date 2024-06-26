b2f0f0bf1736352f590a15b251b0e661e0094fad
Kamakshya-prasad <94913845+Kamakshya-prasad@users.noreply.github.com>
2024-06-26 14:20:21 +0530

    Add files via upload

---
+++ REG-FORM/script1.js
@@ -0,0 +1,17 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `
+        <h2>Registration Data</h2>
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Password:</strong> ${password}</p>
+    `;
+});
